273026db9090c22306fe7a2cbb3fe2070ab5ac92
Namuna Kassaye <your_email_address@example.com>
2022-05-17 19:23:08 -0700

    assignment 5 update 3

--- app/src/androidTest/java/com/example/helloworld/WelcomeTest.java
+++ app/src/androidTest/java/com/example/helloworld/WelcomeTest.java
@@ -4,9 +4,12 @@ package com.example.helloworld;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static com.example.helloworld.RecyclerViewMatcher.withRecyclerView;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -29,8 +32,9 @@ public class WelcomeTest {
         onView(withContentDescription("open navigation drawer")).perform(click());
         onView(withId(R.id.matches_menu_item)).perform(click());
 
-        onView(withId(R.id.matches_menu_item))
-                .check(matches(withText("matches will be here")));
+
+        onView(withRecyclerView(R.id.recycler_view).atPosition(0))
+                .check(matches(hasDescendant(withText("Alex1"))));
 
     }
 }
