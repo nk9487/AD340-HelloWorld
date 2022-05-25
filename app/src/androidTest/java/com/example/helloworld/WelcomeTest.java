d188706c05af38d2f369ef51a1191e353c6602e0
Namuna Kassaye <your_email_address@example.com>
2022-05-24 20:39:44 -0700

    assignment 6 update 03

--- app/src/androidTest/java/com/example/helloworld/WelcomeTest.java
+++ app/src/androidTest/java/com/example/helloworld/WelcomeTest.java
@@ -1,6 +1,17 @@
 
 package com.example.helloworld;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static com.example.helloworld.RecyclerViewMatcher.withRecyclerView;
+
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -17,13 +28,13 @@ public class WelcomeTest {
 
     @Test
     public void clickingOnMatchesDrawerItemDisplaysMatchesFragment() {
-        /*onView(withContentDescription("open navigation drawer")).perform(click());
+        onView(withContentDescription("open navigation drawer")).perform(click());
         onView(withId(R.id.matches_menu_item)).perform(click());
 
         onView(isRoot()).perform(HelpersViewMatcher.waitView(withText("Cool Guy Mike"), 5000));
 
         onView(withRecyclerView(R.id.recycler_view).atPosition(0))
-                .check(matches(hasDescendant(withText("Cool Guy Mike"))));*/
+                .check(matches(hasDescendant(withText("Cool Guy Mike"))));
 
     }
 
